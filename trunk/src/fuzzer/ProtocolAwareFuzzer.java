771748c2562a418e6413bd54e05e14eec5ff1b8b
wadealex <wadealex@6a6773ba-0502-11de-9fdb-3debd5008f28>
2009-03-03 02:44:34 +0000

    Created initial implementation for Fuzzer base class.

--- trunk/src/fuzzer/ProtocolAwareFuzzer.java
+++ trunk/src/fuzzer/ProtocolAwareFuzzer.java
@@ -1,7 +1,50 @@
+/**
+ * 
+ */
 package fuzzer;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ *
+ */
 public class ProtocolAwareFuzzer extends Fuzzer {
 
+	/**
+	 * @param executable
+	 * @param inputImageFilename
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	public ProtocolAwareFuzzer(String executable, String inputImageFilename)
+			throws FileNotFoundException, IOException {
+		super(executable, inputImageFilename);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see fuzzer.Fuzzer#logExecutionParameters()
+	 */
+	@Override
+	protected void logExecutionParameters() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see fuzzer.Fuzzer#writeToOutputFile(java.io.File)
+	 */
+	@Override
+	protected void writeToOutputFile(File outputFile) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
